3fd16c531c60acfb82ae6b0a1385c405377b60ba
Alexei Sholik <alcosholik@gmail.com>
2014-09-10 20:03:13 +0300

    Update Elixir version req to support 1.x

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Benchfella.Mixfile do
   def project do
     [app: :benchfella,
      version: "0.0.2",
-     elixir: "~> 0.15"]
+     elixir: ">= 0.15.0 and < 2.0.0"]
   end
 
   def application do
