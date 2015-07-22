1c7bb751fdc8313c72972bc4883677039ec39284
Aleksei Magusev <lexmag@me.com>
2015-07-22 19:33:15 +0200

    Simplify `Util.read_all_input/0` definition

--- lib/benchfella/cli_util.ex
+++ lib/benchfella/cli_util.ex
@@ -1,9 +1,5 @@
 defmodule Benchfella.CLI.Util do
-  def read_all_input() do
-    read_all_input([])
-  end
-
-  def read_all_input(lines) do
+  def read_all_input(lines \\ []) do
     case IO.binread(:line) do
       :eof ->
         lines |> Enum.reverse |> Enum.join("")
