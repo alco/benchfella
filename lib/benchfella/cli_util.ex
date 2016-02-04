alco/benchfella

commit 2fbe632d6944c3dc20fa930967ab9577d2bb4dd3

Improve `Benchfella.CLI.Util.locate_snapshots/1` - fix the usage when there is only one snapshot in the directory - simplify generation of the snapshots list

lib/benchfella/cli_util.ex
@@ -17,12 +17,11 @@ defmodule Benchfella.CLI.Util do
   def locate_snapshots(count \\ 2) do
     dir = "bench/snapshots"
     snapshots =
-      case File.ls(dir) do
-        {:error, _}  -> []
-        {:ok, files} ->
-          files |> Enum.sort |> Enum.reverse |> Enum.take(count) |> Enum.reverse
-      end
-      |> Enum.map(&Path.join(dir, &1))
+      Path.join(dir, "*.snapshot")
+      |> Path.wildcard
+      |> Enum.sort(&(&1 > &2))
+      |> Enum.take(count)
+
     if snapshots == [] do
       Mix.raise "No snapshots found. Pass - to read from stdin"
     end

commit 6876ba77f540947b9d87b8d217d6109e9e24b558

Fix the order of snapshots in the bench.cmp comparison
This was a regression in 2fbe632d6944c3dc20fa930967ab9577d2bb4dd3

lib/benchfella/cli_util.ex
@@ -15,8 +15,9 @@ defmodule Benchfella.CLI.Util do
     snapshots =
       Path.join(dir, "*.snapshot")
       |> Path.wildcard
-      |> Enum.sort(&(&1 > &2))
+      |> Enum.sort(& &1 > &2)
       |> Enum.take(count)
+      |> Enum.reverse
 
     if snapshots == [] do
       Mix.raise "No snapshots found. Pass - to read from stdin"