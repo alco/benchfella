e0ed3f36a9de6572139e63a6c5a8e5601d99c1cf
Alexei Sholik <alcosholik@gmail.com>
2016-02-16 16:17:22 +0200

    Version 0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Benchfella.Mixfile do
   def project do
     [
       app: :benchfella,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.0",
       description: description,
       package: package,
